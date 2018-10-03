785b5136abd914ba41073c34c4a5114f9ac3208b
Randy <randyma01@gmail.com>
2018-10-03 15:27:17 -0600

    trabajo en interfaz

--- codigo_aplicacion/app.py
+++ codigo_aplicacion/app.py
@@ -1,29 +1,121 @@
-#Se importan todas las bibliotecas necesarias para poder trabajar
-from tkinter import *           #Biblioteca para interfaces gráficas
-import time                     #Biblioteca que interactúa con la hora del S.O.
-import random                   #Biblioteca para generar salidas de forma aleatoria
-import os                       #Biblioteca que permite interactuar con el sistema operativo
-import winsound                 #Biblioteca necesaria para reproducir audios de windows
-import pygame                   #Biblioteca necesaria para la parte lógica del juego
-from tkinter import messagebox
+"""
+----------------------------------------------------------------------
 
-from Game import *
+Area Academica de Administracion de Tecnologia de Informacion
+
+Curso:
+    Bases de Datos Avanzados
+
+Tarea:
+    Laboratorio MongoDB
+
+Fecha:
+    7 de octubre, 2018
+    Semestre II
+
+Estudiante:
+	Randy Martinez Sandi
+
+Carnet: 2014047395
+
+Herramientas Utilizadas:
+    -Python 3.0.7
+    -tkinter
+    -pymongo
+----------------------------------------------------------------------
+"""
+#-------------------------------------------------------------------#
+#-----------------------Bilbiotecas Utilizadas----------------------#
+#-------------------------------------------------------------------#
+import tkinter as tk
+from tkinter import *
+import pymongo
+import os
+
+#-------------------------------------------------------------------#
+#------------------------Parte de MongoDB---------------------------#
+#-------------------------------------------------------------------#
 
 
-#Carga de imagenes
-def CargarImagen(Nombre):
-    ruta    = os.path.join('Imagenes',Nombre)   #Busca la imagen dentro del sistema operativo
-    imagen  = PhotoImage(file=ruta)
+#-------------------------------------------------------------------#
+#----------------------Funciones Auxiliares-------------------------#
+#-------------------------------------------------------------------#
+
+# Cargar Imagenes #
+def cargarImagen(nombre):
+    ruta = os.path.join('imagenes',nombre)
+    imagen = PhotoImage(file=ruta)
     return imagen
 
-def cerrarPrincipal():
-    VentanaPrincipal.destroy()
-    winsound.PlaySound(None, winsound.SND_ASYNC)
+# Terminar el Programa #
+def salida():
+    return exit
+
+#-------------------------------------------------------------------#
+#--------------------------Parte Grafica----------------------------#
+#-------------------------------------------------------------------#
 
 
-#Creación de la ventana principal
-VentanaPrincipal = Tk()
-VentanaPrincipal.title("Ticopoly")               #Se añade un título a la ventana principal
-VentanaPrincipal.minsize(900,650)                #Se establece un tamaño mínimo para la ventana
-VentanaPrincipal.maxsize(900,650)                #Se establece un tamaño máximo para la ventana
-VentanaPrincipal.resizable(width=YES,height=YES)   #Se establece que la ventana no sea expandible
+#-----Ventana de Consultas-----#
+def ventanaConsultas():
+    ventanaPrincipal.withdraw()
+    ventanaConsultas = Toplevel()
+    ventanaConsultas.title("Consultas a MovieDB")
+    ventanaConsultas.minsize(1000,600)
+    ventanaConsultas.resizable(width=NO,height=NO)
+
+    # Imagen de Fondo Ventana Consultas #
+    imagenConsultas = cargarImagen("sala_cine.gif")
+    imagenVentanaConsultas=Label(ventanaConsultas, image=imagenConsultas, bg = "#000000")
+    imagenVentanaConsultas.place (x=0, y=0)
+
+    # Canva Consultas #
+    contenedorConsultas = Canvas(ventanaConsultas , width= 1000, height = 600, bg = "#000000")
+    contenedorConsultas.place(x=0,y=0)
+
+    # Titulo Consultas #
+    tituloConsultas = Label(ventanaConsultas,text="Consultas", fg="#FFFFFF", bg ="#000000", font=("Courier",40))
+    tituloConsultas.place(x=270,y=25)
+
+    # Opciones de Consulta #
+    infoPeliculaNombre = Label(ventanaConsultas,text="-> Consultar pelicula por nombre", fg="#FFFFFF", bg ="#000000", font=("Courier",16))
+    infoPeliculaNombre.place(x=25,y=100)
+
+    # Funcion para regresar a Ventana Principal 
+    def regresarVentanaPrincipal():
+        ventanaConsultas.destroy()
+        ventanaPrincipal.deiconify()
+
+    # Boton para regresar a la Ventana Principal #
+    botonReresoVentanaPrincipal = Button(ventanaConsultas, command=regresarVentanaPrincipal, text="SALIDA", bg="#FFFFFF", fg="#FE0000", font=("Courier",28))
+    botonReresoVentanaPrincipal.place(x=345,y=275)
+
+
+#-----Ventana Principal-----#
+ventanaPrincipal = Tk()
+ventanaPrincipal.title("MovieDB")
+ventanaPrincipal.minsize(800,600)
+ventanaPrincipal.resizable(width=NO,height=NO)
+ventanaPrincipal.geometry("800x600+250+50")
+
+# Canva Principal #
+contenedorPrincipal = Canvas(ventanaPrincipal , width= 800, height = 700, bg = "#000000")
+contenedorPrincipal.place(x=0,y=0)
+
+# Imagen de Fondo Ventana Principal #
+imagenPrincipal = cargarImagen("entrada_cine.gif")
+imagenVentanaPrincipal=Label(ventanaPrincipal, image=imagenPrincipal, bg = "#FFFFFF")
+imagenVentanaPrincipal.place (x=0, y=0)
+
+# Titulo Ventana Principal #
+tituloPrincipal = Label(ventanaPrincipal,text="MovieDB", fg="#000000", bg ="#FFFFFF", font=("Courier",55))
+tituloPrincipal.place(x=285,y=95)
+
+# Boton para ir a Ventana de Consultas #
+botonConsultas = Button(ventanaPrincipal, command=ventanaConsultas, text="Consultas", bg = "#000000", fg = "#000000", font=("Courier",30))
+botonConsultas.place(x=315,y=225)
+
+# Boton para terminar del Programar #
+botonSalida = Button(ventanaPrincipal, command=salida(), text="SALIDA", bg="#FFFFFF", fg="#FE0000", font=("Courier",28))
+botonSalida.place(x=345,y=275)
+
